6551e1d563ab88b28342d1ee6cfc3a5421b01885
s4sachin <sachinpurohit.text@google.com>
2024-07-23 08:41:04 +0530

    test: add test for reject

--- js-promises/promise-all-polyfill.js
+++ js-promises/promise-all-polyfill.js
@@ -25,6 +25,8 @@ const myPromiseAll = (promisesArray) => {
 }
 
 /* Testing */
+
+// Test Case 1
 function task(time) {
   return new Promise(function (resolve, reject) {
     setTimeout(function () {
@@ -40,3 +42,24 @@ myPromiseAll(taskList)
   .catch(console.error);
 //Output:
 //"got results" [1000,5000,3000]
+
+// Test Case 2
+function task2(time) {
+  return new Promise(function (resolve, reject) {
+    setTimeout(function () {
+      if(time < 3000){
+        reject("Rejected");
+      }else{
+        resolve(time);
+      }
+}, time); });
+}
+const taskList2 = [task2(1000), task2(5000), task2(3000)];
+//run promise.all
+myPromiseAll(taskList2)
+  .then(results => {
+    console.log("got results", results)
+  })
+  .catch(console.error);
+// Output:
+// "Rejected"
