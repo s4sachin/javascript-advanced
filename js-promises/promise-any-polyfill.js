bc09a8a30e268392ab823f06f2dcdc0d1eee965b
s4sachin <sachinpurohit.text@google.com>
2024-07-24 07:38:56 +0530

    fix: the order of result promise return && add test
    The promises error returned were in the order of original array and not
    in the order of resolved. Also added test cases to test

--- js-promises/promise-any-polyfill.js
+++ js-promises/promise-any-polyfill.js
@@ -9,7 +9,7 @@ const myPromiseAny = (promisesArray) => {
       promise
         .then((val) => resolve(val))
         .catch((err) => {
-          errorResult[index] = err;
+          errorResult[promiseCount] = err;
           promiseCount += 1;
           if (errorResult.length === promiseCount) {
             reject(errorResult);
@@ -19,4 +19,44 @@ const myPromiseAny = (promisesArray) => {
   });
 };
 
+/* Test Cases */
 
+//Input:
+const test1 = new Promise(function (resolve, reject) {
+  setTimeout(reject, 500, 'one');
+});
+const test2 = new Promise(function (resolve, reject) {
+  setTimeout(resolve, 600, 'two');
+});
+const test3 = new Promise(function (resolve, reject) {
+  setTimeout(reject, 200, 'three');
+});
+myPromiseAny([test1, test2, test3]).then(function (value) {
+  // first and third fails, 2nd resolves
+  console.log(value);
+}).catch(function (err){
+  console.log(err);
+});
+// Expected Output:
+// "two"
+
+
+
+//Input:
+const test4 = new Promise(function (resolve, reject) {
+setTimeout(reject, 500, 'one');
+});
+const test5 = new Promise(function (resolve, reject) {
+  setTimeout(reject, 600, 'two');
+});
+const test6 = new Promise(function (resolve, reject) {
+  setTimeout(reject, 200, 'three');
+});
+myPromiseAny([test4, test5, test6]).then(function (value) {
+  console.log(value);
+}).catch(function (err){
+  // all three fails
+  console.log(err);
+});
+// Expected Output:
+// ["three","one","two"]
